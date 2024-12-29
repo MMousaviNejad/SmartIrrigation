8f320e1a2b413c63a6c157072728880c917a04c5
Mojtaba Mosavi <prince.18.old@gmail.com>
2024-12-29 03:45:16 +0330

    bug fixed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
     public ContentResult JSON()
     {
 
-        var dataPoints = _context.MoistureDatas.Select(x => new DataPoint(x.Timestamp.Ticks / TimeSpan.TicksPerSecond, x.Moisture)).ToList();
+        var dataPoints = _context.MoistureDatas.Select((x,i) => new DataPoint(i, x.Moisture)).ToList();
 
         JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
         return Content(JsonConvert.SerializeObject(dataPoints, _jsonSetting), "application/json");
